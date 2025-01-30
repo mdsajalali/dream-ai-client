c52224613599f04e2f7ad2132e74ba6785d056a2
mdsajalali <mdsajalali2020@gmail.com>
2025-01-30 21:41:12 +0600

    get: all images

--- src/components/shared/ImageCard.tsx
+++ src/components/shared/ImageCard.tsx
@@ -5,15 +5,18 @@ import { Download, Share2, Copy, Heart } from "lucide-react";
 
 const ImageCard = ({ image }: ImageCardProps) => {
   return (
-    <div key={image.id} className="bg-white p-4 rounded-lg shadow-lg">
+    <div key={image._id} className="bg-white p-4 rounded-lg shadow-lg">
       <img
-        src={image.url}
+        src={image?.imageUrl}
         alt="Generated"
         className="w-full h-48 object-cover rounded-lg"
       />
       <div className="mt-3 flex items-center justify-between">
         <p className="text-sm text-gray-600">Created by: {image.creator}</p>
         <p className="text-sm text-gray-500">{image.createdAt}</p>
+      </div>
+      <div>
+        <p className="text-sm text-gray-600"> {image.prompt}</p>
       </div>
       <div className="flex items-center gap-4 mt-3">
         <Download className="w-5 h-5 cursor-pointer text-gray-600 hover:text-black" />
