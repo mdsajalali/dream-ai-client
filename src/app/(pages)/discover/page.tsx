169d599470525839d69126b9cc8a248035aea158
mdsajalali <mdsajalali2020@gmail.com>
2025-01-30 11:40:32 +0600

    update: dicover ui

--- src/app/(pages)/discover/page.tsx
+++ src/app/(pages)/discover/page.tsx
@@ -1,22 +1,9 @@
-import React from "react";
 import { Download, Share, Copy, Heart } from "lucide-react";
 
 const Discover = () => {
   return (
     <div className="min-h-[calc(100vh-100px)] flex flex-col items-center justify-center p-4">
       <div className="w-full max-w-md bg-gray-800 p-6 rounded-lg shadow-xl">
-        {/* Search Input */}
-        <div className="flex items-center gap-2 mb-4">
-          <input
-            type="text"
-            placeholder="This is Discover what you want to generate"
-            className="flex-1 p-3 text-sm rounded-md border border-gray-600 bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <button className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">
-            Generate
-          </button>
-        </div>
-
         {/* Image Placeholder */}
         <div className="relative w-full h-64 bg-gray-700 flex items-center justify-center rounded-md overflow-hidden shadow-md">
           <img
@@ -41,6 +28,18 @@ const Discover = () => {
             </button>
           </div>
         </div>
+      </div>
+
+      {/* Search Input */}
+      <div className="flex items-center gap-2 mt-4 ">
+        <input
+          type="text"
+          placeholder="This is Discover what you want to generate"
+          className="flex-1 p-2 text-sm rounded-md border border-gray-600 bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">
+          Generate
+        </button>
       </div>
     </div>
   );
