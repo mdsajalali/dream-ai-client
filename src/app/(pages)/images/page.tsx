42ae545570270f524a5084a0f58fbb5f156e5027
mdsajalali <mdsajalali2020@gmail.com>
2025-01-31 16:23:34 +0600

    add: lightgallery

--- src/app/(pages)/images/page.tsx
+++ src/app/(pages)/images/page.tsx
@@ -4,6 +4,16 @@ import Container from "@/components/shared/Container";
 import ImageCard from "@/components/shared/ImageCard";
 import axiosInstance from "@/utils/axiosInstance";
 
+import LightGallery from "lightgallery/react";
+import lgZoom from "lightgallery/plugins/zoom";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+
+// Import styles
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+import { SingleImageCardProps } from "@/types/index.type";
+
 const ImagesPage = () => {
   const [images, setImages] = useState([]);
 
@@ -18,11 +28,18 @@ const ImagesPage = () => {
 
   return (
     <Container>
-      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {images?.map((image, idx) => (
-          <ImageCard key={idx} image={image} />
+      <LightGallery
+        plugins={[lgZoom, lgThumbnail]}
+        speed={500}
+        elementClassNames="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6"
+        // mode="lg-fade"
+      >
+        {images?.map((image: SingleImageCardProps) => (
+          <a key={image?._id} href={image?.imageUrl} data-src={image?.imageUrl}>
+            <ImageCard image={image} />
+          </a>
         ))}
-      </div>
+      </LightGallery>
     </Container>
   );
 };
--- src/types/index.type.ts
+++ src/types/index.type.ts
@@ -1,10 +1,11 @@
-// Image Cart type
 export interface ImageCardProps {
-  image: {
-    _id: string;
-    imageUrl: string;
-    creator: string;
-    createdAt: string;
-    prompt: string;
-  };
+  image: SingleImageCardProps;
+}
+
+export interface SingleImageCardProps {
+  _id: string;
+  imageUrl: string;
+  creator: string;
+  createdAt: string;
+  prompt: string;
 }
